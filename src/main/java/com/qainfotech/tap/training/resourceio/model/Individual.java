95c9ba4507e9af10241cb8c7c4adec7341adfe82
SomilBansalQA <somilbansal@qainfotech.com>
2017-06-27 17:24:52 +0530

    Update Individual.java
    Added Comments.

--- src/main/java/com/qainfotech/tap/training/resourceio/model/Individual.java
+++ src/main/java/com/qainfotech/tap/training/resourceio/model/Individual.java
@@ -7,55 +7,48 @@ import java.util.Map;
  * @author Ramandeep RamandeepSingh AT QAInfoTech.com
  */
 public class Individual {
-    
-    private final String name;
-    private final Integer id;
-    private final Boolean active;
-    
-    public Individual(Map<String, Object> individualMap){
-     //   throw new UnsupportedOperationException("Not implemented.");
-        String name1= null;
-		Integer id1= null;
-		Boolean active1= null;
-		for (Map.Entry<String, Object> entry : individualMap.entrySet()) {
-			if (entry.getKey() == "name")
-				name1 = entry.getValue().toString();
-			if (entry.getKey() == "id")
-				id1 = (Integer) entry.getValue();
-			if (entry.getKey() == "active")
-				active1 = (Boolean) entry.getValue();
 
-		}
-		name = name1;
-		id = id1;
-		active = active1;
-	//	    System.out.println(name+" "+id+" "+active);  
-		 
-    }
-    
-    /**
-     * get the name of individual
-     * 
-     * @return individual name
-     */
-    public String getName(){
-        return name;
-    }
-    
-    /**
-     * get the employee of of individual
-     * @return id
-     */
-    public Integer getId(){
-        return id;
-    }
-    
-    /**
-     * get the active status of individual
-     * 
-     * @return 
-     */
-    public Boolean isActive(){
-        return active;
-    }
+	private final String name;
+	private final Integer id;
+	private final Boolean active;
+
+	/**
+	 * 
+	 * @param individualMap
+	 */
+	//Constructor
+	public Individual(Map<String, Object> individualMap) {
+		// throw new UnsupportedOperationException("Not implemented.");
+
+		this.name = individualMap.get("name").toString();
+		this.id = Integer.parseInt(individualMap.get("id").toString());
+		this.active = (Boolean) individualMap.get("active");
+	}
+
+	/**
+	 * get the name of individual
+	 * 
+	 * @return individual name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * get the employee of of individual
+	 * 
+	 * @return id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * get the active status of individual
+	 * 
+	 * @return
+	 */
+	public Boolean isActive() {
+		return active;
+	}
 }
