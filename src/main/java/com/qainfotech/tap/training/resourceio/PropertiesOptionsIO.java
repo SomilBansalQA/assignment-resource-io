1d041fc90e00b21d3e274e7843784e6a04404a0f
SomilBansalQA <somilbansal@qainfotech.com>
2017-06-27 17:23:00 +0530

    Update PropertiesOptionsIO.java
    Added Comments.

--- src/main/java/com/qainfotech/tap/training/resourceio/PropertiesOptionsIO.java
+++ src/main/java/com/qainfotech/tap/training/resourceio/PropertiesOptionsIO.java
@@ -1,33 +1,57 @@
 package com.qainfotech.tap.training.resourceio;
 
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Properties;
 
 /**
  *
  * @author Ramandeep RamandeepSingh AT QAInfoTech.com
  */
-public class PropertiesOptionsIO{
-    
-    public Object getOptionValue(String optionKey) throws IOException {
-        //throw new UnsupportedOperationException("Not implemented.");
+public class PropertiesOptionsIO {
+	/**
+	 * 
+	 * @param optionKey
+	 * @return
+	 * @throws IOException
+	 */
+	public Object getOptionValue(String optionKey) throws IOException {
+		// throw new UnsupportedOperationException("Not implemented.");
 
-     FileReader reader=new FileReader("C:\\Users\\somilbansal\\Desktop\\Eclipse_Workspace\\assignment-resource-io-master1\\src\\test\\resources\\options.properties"); 
+		// Reading Option.properties File from main resources
+		FileReader reader = new FileReader(
+				"C:\\Users\\somilbansal\\Desktop\\Eclipse_Workspace\\assignment-resource-io-master1\\src\\main\\resources\\options.properties");
 
-    	Properties p=new Properties(); 
-    	p.load(reader); 
+		Properties p = new Properties();
+		p.load(reader);
 
-    	return p.getProperty(optionKey);}
+		return p.getProperty(optionKey);
+	}
 
-    public void addOption(String optionKey, Object optionValue) throws IOException {
-      //  throw new UnsupportedOperationException("Not implemented.");
-    FileReader reader=new FileReader("C:\\Users\\somilbansal\\Desktop\\Eclipse_Workspace\\assignment-resource-io-master1\\src\\test\\resources\\options.properties"); 
+	/**
+	 * 
+	 * @param optionKey
+	 * @param optionValue
+	 * @throws IOException
+	 */
+	public void addOption(String optionKey, Object optionValue) throws IOException {
+		// throw new UnsupportedOperationException("Not implemented.");
 
-    	Properties p=new Properties(); 
-    	p.load(reader); 
+		FileReader reader = new FileReader(
+				"C:\\Users\\somilbansal\\Desktop\\Eclipse_Workspace\\assignment-resource-io-master1\\src\\main\\resources\\options.properties");
 
-    	p.setProperty(optionKey,(String) optionValue);
+		Properties p = new Properties();
+		p.load(reader);
 
-    	p.store(new FileWriter("C:\\Users\\somilbansal\\Desktop\\Eclipse_Workspace\\assignment-resource-io-master1\\src\\test\\resources\\options.properties"),"Written");
+	
+		p.setProperty(optionKey, (String) optionValue);
 
-    }
+		// Writing in Option.properties File
+
+		p.store(new FileWriter(
+				"C:\\Users\\somilbansal\\Desktop\\Eclipse_Workspace\\assignment-resource-io-master1\\src\\main\\resources\\options.properties"),
+				"");
+
+	}
 }
